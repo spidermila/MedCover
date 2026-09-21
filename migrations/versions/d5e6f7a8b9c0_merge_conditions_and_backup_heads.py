2c7975472c890bdefd9686f089a880452f89440a
Lumír Balhar <frenzy.madness@gmail.com>
2026-09-21 12:08:25 +0000

    chore: merge migration heads after rebase

---
+++ migrations/versions/d5e6f7a8b9c0_merge_conditions_and_backup_heads.py
@@ -0,0 +1,14 @@
+"""Merge the conditions and backup migration branches."""
+
+revision = "d5e6f7a8b9c0"
+down_revision = ("b2c3d4e5f6a7", "c4e7a1d8b6f2")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
